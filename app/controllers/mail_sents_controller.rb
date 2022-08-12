3e4ac188bfe3fd0aec7f92472cf40878757fd81d
midven <vincent.midlaire@gmail.com>
2022-08-12 09:50:55 +0000

    multi recipients

--- app/controllers/mail_sents_controller.rb
+++ app/controllers/mail_sents_controller.rb
@@ -24,25 +24,54 @@ class MailSentsController < ApplicationController
         @message_id = params[:message_id]
         @message = Message.find(@message_id)
         
+        puts @recipients = mail_params[:recipient].to_s.split
         
-        @mail = MailSent.new(mail_params)
-        @mail.user_id = 1
-        @mail.message_id = @message_id
+        
+        @recipients.each do |recipient|
+            @validation_mail = recipient =~ URI::MailTo::EMAIL_REGEXP
+            
+            puts '^^^^^^^^^^^^^^^^^^'
+            puts @validation_mail
+            puts '^^^^^^^^^^^^^^^^^^'
+
+            if @validation_mail == nil
+                flash[:danger] = "Some mails are invalid"
+                redirect_to new_message_mail_sent_path and return
+            end
+        end
+        
+        
+        
+        @recipients.each do |recipient|
+        
+            @mail = MailSent.new(mail_params)
+            @mail.user_id = 1
+            @mail.message_id = @message_id
+            @mail.recipient = recipient
+            @mail.save
+            
+            if @recipients.last == recipient
+                
+                if @mail.save
+                    MailSentMailer.send_mail(@mail).deliver_now
+                    
+                    redirect_to message_mail_sents_path
+                    
+                    flash[:notice] = "Your mail was sended successfully."
+                else
+                    render 'new'
+                end
+            else
+                MailSentMailer.send_mail(@mail).deliver_now
+            end
+        end
         
         puts '----------------------'
         puts params
+        puts @recipients
         puts '----------------------'
 
 
-        if @mail.save
-            MailSentMailer.send_mail(@mail).deliver_now
-            
-            redirect_to message_mail_sents_path
-            
-            flash[:notice] = "Your mail was sended successfully."
-        else
-            render 'new'
-        end
     end
     
     def show
@@ -55,6 +84,10 @@ class MailSentsController < ApplicationController
         def mail_params
             params.require(:mail_sent).permit(:title, :content, :recipient)
         end
+        
+        # def is_email_valid? email
+        #     email =~ URI::MailTo::EMAIL_REGEXP
+        # end
 
 
 end
